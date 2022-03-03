c7e778056ac886114d7fa1d46caae2f3d7801f22
milske <millanerg@gmail.com>
2022-03-03 22:22:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Learn React
+      <a href="#" className="btn btn-primary">
+        Hello
+      </a>
+    </div>
+  );
+}
+
+export default App;
