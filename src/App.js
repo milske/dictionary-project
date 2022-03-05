62ccdea18a542182503552482f84b2e2a95ab681
milske <millanerg@gmail.com>
2022-03-05 17:34:53 +0100

    Added github link to the page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ export default function App() {
       <div className="App">
         <Dictionary />
       </div>
+      <footer className="link">
+        Coded by Milla,{" "}
+        <a href="https://github.com/milske/dictionary-project">
+          found in GitHub
+        </a>
+      </footer>
     </div>
   );
 }
